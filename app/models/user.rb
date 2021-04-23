a9f528f12b80ab9820e809837c90f319e93a01cd
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-04-23 19:59:20 +0900

    エラーログの表示の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :birthday, presence: true
+with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'Full-width characters'} do
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_katakana, presence: true
   validates :first_name_katakana, presence: true
-  validates :birthday, presence: true
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end
