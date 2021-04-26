8b36dcfd94b40e1805ecb97e2013b893c278d528
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-04-26 20:47:08 +0900

    各ActiveHashの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  belong_to :user
+  belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
       belongs_to :category
