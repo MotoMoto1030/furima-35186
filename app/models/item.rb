cbcb246ff04884c5a75aebca79b8226d94da4a93
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-04-26 14:56:09 +0900

    アソシエーションの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belong_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+      belongs_to :category
+      belongs_to :days_to_ship
+      belongs_to :item_status
+      belongs_to :shipping_area
+      belongs_to :shipping_charge
 end
