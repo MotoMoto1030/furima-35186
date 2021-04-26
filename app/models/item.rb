7b1648a71140c8cdf1609b95ff140690698f991e
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-04-26 16:21:59 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,19 @@ class Item < ApplicationRecord
       belongs_to :item_status
       belongs_to :shipping_area
       belongs_to :shipping_charge
+    
+    with_options presence: true do
+      validates :item_name
+      validates :item_description
+        with_options numericality: { other_than: 1 } do
+          validates :category_id
+          validates :days_to_ship_id
+          validates :item_status_id
+          validates :shipping_area_id
+          validates :shipping_charge_id
+        end
+            with_options format: { with:/\A[0-9]+\z/, message: 'Price Half-width number'} do
+              validates :price
+            end
+    end
 end
