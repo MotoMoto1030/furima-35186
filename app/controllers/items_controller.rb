154ebe467a6e84a01a920efb4449ee458f437428
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-04-26 16:52:42 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,12 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
