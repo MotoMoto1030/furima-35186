f1ec97c10cbe5a646f2de547943530829a7a477b
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-04-28 12:31:52 +0900

    itemの単体テストコード(Rubocop済)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image,:item_name,:item_description,:price,:item_status_id,:shipping_charge_id,:shipping_area_id,:category_id,:days_to_ship_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_description, :price, :item_status_id, :shipping_charge_id, :shipping_area_id,
+                                 :category_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '出品できるとき' do
+      it '必要な情報が全て存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できない時' do
+      it 'imageが空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'item_nameが空では出品できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+
+      it 'item_descriptionが空では出品できない' do
+        @item.item_description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description can't be blank")
+      end
+
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが全角数字だと出品できない' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Half-width number')
+      end
+
+      it 'priceが範囲内の数字でなければ出品できない' do
+        @item.price = '100'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price Out of setting range')
+      end
+
+      it 'item_statusが空では出品できない' do
+        @item.item_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item status Select')
+      end
+
+      it 'shipping_chrgeが空では出品できない' do
+        @item.shipping_charge_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping charge Select')
+      end
+
+      it 'shipping_areaが空では出品できない' do
+        @item.shipping_area_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Shipping area Select')
+      end
+
+      it 'categoryが空では出品できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category Select')
+      end
+
+      it 'days_to_shipが空では出品できない' do
+        @item.days_to_ship_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Days to ship Select')
+      end
+    end
+  end
 end
