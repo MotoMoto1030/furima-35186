43538777adf467908865e6be7c1c6f99210485a9
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-04-30 21:12:42 +0900

    投稿者専用ページに遷移させない実装。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @items = Item.all.order(id: 'DESC')
   end
@@ -17,15 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
        redirect_to item_path
     else
@@ -38,5 +41,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :item_name, :item_description, :price, :item_status_id, :shipping_charge_id, :shipping_area_id,
                                  :category_id, :days_to_ship_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
