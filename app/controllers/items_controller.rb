78d1f1349086a812a7bdc2f1bbf92c501c6ff868
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-01 12:10:25 +0900

    商品削除機能(Rubocop済)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,10 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-       item = Item.find(params[:id])
-    if item.destroy
-      redirect_to root_path
-    end
+    item = Item.find(params[:id])
+    redirect_to root_path if item.destroy
   end
 
   private
