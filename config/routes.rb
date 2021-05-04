ad7d03306e3fdcb6dfc15e79eda57b4ba6cec1ac
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-04 18:32:07 +0900

    記述間違えの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items do
-    references :orders, only: [:index, :create]
+    resources :orders, only: [:index, :create]
+  end
 end
