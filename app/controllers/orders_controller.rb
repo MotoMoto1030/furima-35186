3940ab80127ed4084758a6b36c63d3117b9f6049
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-05 16:51:08 +0900

    フォームの情報変更

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :municipality, :address, :phone_number, :shipping_area_id, :building_name, :item_id).merge(user_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_05_04_063308) do
 
   create_table "address_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
-    t.integer "prefecture_id", null: false
+    t.integer "shipping_area_id", null: false
     t.string "municipality", null: false
     t.string "address", null: false
     t.string "building_name"
