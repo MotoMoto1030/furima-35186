207a3da37b62fca63458e4c9d2dd7220b1f86eb2
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-05 15:17:44 +0900

    ActiveHashの連係、正しい表示の変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :address_info
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipping_area
 end
