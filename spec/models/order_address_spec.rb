34d5b7d79269fc5ac730094ae9f4cb3366125e59
MotoMoto1030 <motosawa@Motos-MacBook-Pro.local>
2021-05-10 16:48:30 +0900

    各単体テストコードを日本語へ書き換え

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -24,69 +24,69 @@ RSpec.describe OrderAddress, type: :model do
       it 'postal_codeが空だと保存できないこと' do
         @order_address.postal_code = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+        expect(@order_address.errors.full_messages).to include("郵便番号を入力してください")
       end
 
       it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
         @order_address.postal_code = '9999999'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include('Postal code Input correctly')
+        expect(@order_address.errors.full_messages).to include('郵便番号はハイフンを含め半角数字で入力してください')
         sleep 0.1
       end
 
       it 'municipalityが空だと保存できないこと' do
         @order_address.municipality = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Municipality can't be blank")
+        expect(@order_address.errors.full_messages).to include("市区町村を入力してください")
         sleep 0.1
       end
 
       it 'addressが空だと保存できないこと' do
         @order_address.address = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Address can't be blank")
+        expect(@order_address.errors.full_messages).to include("番地を入力してください")
         sleep 0.1
       end
 
       it 'phone_numberが空だと保存できないこと' do
         @order_address.phone_number = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+        expect(@order_address.errors.full_messages).to include("電話番号を入力してください")
         sleep 0.1
       end
 
       it 'phone_numberが全角数字だと保存できないこと' do
         @order_address.phone_number = '０９０５５５５１１１０'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include('Phone number Input only number')
+        expect(@order_address.errors.full_messages).to include('電話番号は数字のみ入力してください')
         sleep 0.1
       end
 
       it 'shipping_area_idが1だと保存できないこと' do
         @order_address.shipping_area_id = 1
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include('Shipping area Select')
+        expect(@order_address.errors.full_messages).to include('都道府県を選択してください')
         sleep 0.1
       end
 
       it 'userが空だと保存できないこと' do
         @order_address.user_id = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("User can't be blank")
+        expect(@order_address.errors.full_messages).to include("ユーザー情報を入力してください")
         sleep 0.1
       end
 
       it 'itemが空だと保存できないこと' do
         @order_address.item_id = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+        expect(@order_address.errors.full_messages).to include("商品情報を入力してください")
         sleep 0.1
       end
 
       it 'tokenが空だと保存できないこと' do
         @order_address.token = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+        expect(@order_address.errors.full_messages).to include("カード情報を入力してください")
         sleep 0.1
       end
     end
